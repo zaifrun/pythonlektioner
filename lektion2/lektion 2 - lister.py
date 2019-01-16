1d8d9f9113dd001599f0473b680e2e3049f1ed12
makn <makn@eaaa.dk>
2019-01-16 14:12:20 +0100

    nye kommentarer

--- lektion2/lektion 2 - lister.py
+++ lektion2/lektion 2 - lister.py
@@ -5,5 +5,39 @@ print("lektion 2 - lister")
 
 minliste = []
 
-minliste2 = ["martin","benny"]
+navne = ["martin","benny"]
 
+
+#udskrivning af lister
+print(minliste)
+print(navne)
+
+#tilgå enkelte elementer
+
+
+#modificer elementer
+
+
+#tilføje nye elementer til slutningen
+
+
+#tilføje elementer et sted i listen med insert
+
+
+#slet elementer med del kommandoen
+
+
+#remove by value med remove("navn")
+
+#sortering af lister - med list.sort() og sorted(list)
+
+
+#længden af en liste med len
+
+#index -1 - bruges til......:-)
+
+
+#looping through a list
+cars = ["porsche","hyundai","BMW"]
+for car in cars:
+    print(car)
