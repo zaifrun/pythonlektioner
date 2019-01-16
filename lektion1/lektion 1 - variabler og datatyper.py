ad88ecaa9cfa285c6a96b74ec89b8cb8dc3f75db
makn <makn@eaaa.dk>
2019-01-16 10:18:57 +0100

    Initial commit

---
+++ lektion1/lektion 1 - variabler og datatyper.py
@@ -0,0 +1,16 @@
+print("lektion 1 - variabler og datatyper")
+
+# variabler - brug lower case generelt - underscore kan bruges - brug IKKE æøå som variabel navne
+
+
+#strings - definition og konkatenation. New lines i strings.
+
+#metoder på strings
+
+message = "hej med dig"
+
+
+#CTRL-Space er nyttigt for code completion
+
+
+#Integers og Floats - brug af str() funktionen til konvertering
---
+++ lektion2/lektion 2 - lister.py
@@ -0,0 +1,9 @@
+#lektion 2 - arbejde med lister i python
+
+
+#definition af en liste
+
+minliste = []
+
+minliste2 = ["martin","benny"]
+
