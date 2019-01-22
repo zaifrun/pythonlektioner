2f015eda2705944ba38018eafb96a17670dd6741
makn <makn@eaaa.dk>
2019-01-22 13:28:27 +0100

    opdateret med flere eksempler

--- lektion1/lektion 1 - variabler og datatyper.py
+++ lektion1/lektion 1 - variabler og datatyper.py
@@ -1,3 +1,6 @@
+""" Denne fil indeholder eksempel kode på emner fra lektion 1 omkring
+    variabler og datatyper """
+
 print("lektion 1 - variabler og datatyper")
 
 # variabler - brug lower case generelt - underscore kan bruges - brug IKKE æøå som variabel navne
@@ -14,3 +17,11 @@ message = "hej med dig"
 
 
 #Integers og Floats - brug af str() funktionen til konvertering
+
+
+#eksempel på type error - int og string
+
+# matematik med +,-,*,/ i python, samt power **
+
+
+# multi-line kommentar med 3 x "
--- lektion2/lektion 2 - lister.py
+++ lektion2/lektion 2 - lister.py
@@ -1,4 +1,5 @@
-#lektion 2 - arbejde med lister i python
+""" Denne indeholder eksempel kode fra lektion 2 - arbejde med lister"""
+
 print("lektion 2 - lister")
 
 #definition af en liste
@@ -12,32 +13,49 @@ navne = ["martin","benny"]
 print(minliste)
 print(navne)
 
-#tilgå enkelte elementer
+#tilgå enkelte elementer - med [] notation
 
 
 #modificer elementer
 
 
-#tilføje nye elementer til slutningen
+#tilføje nye elementer til slutningen - med append
 
 
 #tilføje elementer et sted i listen med insert
 
 
-#slet elementer med del kommandoen
+#slet elementer med del kommandoen - del list[2
 
 
 #remove by value med remove("navn")
 
-#sortering af lister - med list.sort() og sorted(list)
+#sortering af lister - med list.sort() (ændrer listen) og sorted(list) (ændrer ikke listen)
 
 
 #længden af en liste med len
 
-#index -1 - bruges til......:-)
+#reverse af en liste med list.reverse() - ændrer på listen.
 
+#index -1 - bruges til sidste element
+print( navne[-1] )
 
+""" 
 #looping through a list
 cars = ["porsche","hyundai","BMW"]
 for car in cars:
     print(car)
+"""
+
+# numeriske lister - bemærk sidste index kommer IKKE med
+""" 
+for x in range(1,5):
+    print(x)
+"""
+# definer en liste med tal
+
+"""
+tal = list(range(1,6))
+print(tal)
+"""
+
