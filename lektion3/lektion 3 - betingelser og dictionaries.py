83aaea128403c7dac287ec5cdc04d9a8dd320e3d
makn <makn@eaaa.dk>
2019-01-23 11:08:13 +0100

    lektion 3 update

---
+++ lektion3/lektion 3 - betingelser og dictionaries.py
@@ -0,0 +1,24 @@
+""" Denne indeholder eksempel kode fra lektion 2 - betingelse og dictionaries"""
+
+print("lektion 3 - betingelse og dictionaries")
+
+
+#forskel på = og ==
+print ("######## forskel på = og == ############")
+car = "Fiat"
+print(car == "fiat")
+print(car == "Fiat")
+
+if car == "Fiat":
+    print("Det er en Fiat")
+else:
+    print("Det er ikke en Fiat")
+
+
+# nummeriske sammenligninger
+print(" ###### numeriske sammenligninger ###########3")
+alder = 41
+print (alder<50)
+print (alder>45)
+print (alder!=40)
+print (alder>40 and alder<50)
